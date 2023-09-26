329806fddfba8311bc478646aa5a10b7dc67f92f
Renar Saaremets <58975004+renarsaar@users.noreply.github.com>
2023-09-26 22:44:16 +0300

    #27 Ability to clear all trash notes

--- client/src/layouts/NoteSidebar/NoteSidebarHeader/index.tsx
+++ client/src/layouts/NoteSidebar/NoteSidebarHeader/index.tsx
@@ -1,15 +1,43 @@
+import { useContext } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../hooks/hooks'
+import { ActiveTooltipContext } from '../../../context/ActiveTooltipContext'
+import { selectNavigation } from '../../../store/navigation/navigationSlice'
+import { clearTrashNotes, selectNotes } from '../../../store/notes/notesSlice'
 import './style.scss'
 
-type Props = {}
+const NoteSidebarHeader = () => {
+  const dispatch = useAppDispatch()
+  const { tab } = useAppSelector(selectNavigation)
+  const { clearActiveNoteId } = useContext(ActiveTooltipContext)
+  const notes = useAppSelector(selectNotes)
+  const trashNotes = notes.filter((note) => note.trash === true)
 
-export default function index({ }: Props) {
+  const onClick = () => {
+    dispatch(clearTrashNotes())
+    clearActiveNoteId()
+  }
+
   return (
     <div className='note-sidebar-header'>
       <input
-        data-testid="note-search"
-        type="search"
-        placeholder="Search for notes"
+        data-testid='note-search'
+        type='search'
+        placeholder='Search for notes'
       />
+
+      {(tab === 'trash' && trashNotes.length !== 0) &&
+        <>
+          <button
+            className='empty-trash-button'
+            title='Empty'
+            onClick={onClick}
+          >
+            Empty
+          </button>
+        </>
+      }
     </div>
   )
 }
+
+export default NoteSidebarHeader
